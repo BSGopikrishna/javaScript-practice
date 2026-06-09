0dc6609edce69e9a926c275e3e1356ab02216076
BSGopikrishna <ggopi98102@gmail.com>
2026-06-09 15:02:52 +0530

    Added array.js example

---
+++ Object.js
@@ -0,0 +1,10 @@
+let person ={
+    firstName : "John",
+    lastName :  "kendy",
+    age  : 50,
+    weight : 55,
+}
+console.log(person["firstName"]);
+console.log(person["lastName"]);
+console.log(person.age);
+console.log(person.weight);
