fd9d7beef65830fe7b0bf19a3207fb4d0a4f3d14
BSGopikrishna <ggopi98102@gmail.com>
2026-06-11 08:11:59 +0530

    Update practice code

--- Object.js
+++ Object.js
@@ -2,9 +2,73 @@ let person ={
     firstName : "John",
     lastName :  "kendy",
     age  : 50,
-    weight : 55,
+    weight : 54,
 }
+/*
+//Accessing object properties using
 console.log(person["firstName"]);
 console.log(person["lastName"]);
 console.log(person.age);
 console.log(person.weight);
+
+//Adding new properties to an object
+person.height =5.5;
+console.log(person.height);
+
+person.gender = "nale";
+console.log(person.gender);
+
+//Updating existing properties of an object
+
+person.firstName = "gopi";
+console.log(person.firstName);
+
+person.lastName = "krishna";
+console.log(person.lastName);
+
+person.age = 25;
+console.log(person.age);
+
+person.weight = 55;
+console.log(person.weight);
+
+person.height = 5.4;
+console.log(person.height);
+
+person.gender = "male";
+console.log(person.gender);
+
+//updateing existing properties
+person.weight =56;
+console.log(person.weight);
+
+
+//deleting properties 
+delete person.weight;
+//console.log(person.weight); 
+*/
+
+
+
+
+
+//for in loop in object for rading all the properties of an object
+
+for(let y in person){
+   // console.log(y); //only property name
+   // console.log(person[y]); //property value
+    console.log(y +":\t"+ person[y]); //property name and value
+
+
+}
+
+let employee = {
+    ename : "madhu",
+    email : "email@gmail.com",
+    ejob : "Engineer",
+    baseSalary : 50000,
+    bonus : function(){
+        return((this.baseSalary*10)/100);
+    }
+}
+console.log("The salary increment is : " + employee.bonus());
--- array.js
+++ array.js
@@ -1,2 +1,38 @@
-let fruits = ["Apple", "Banana", "Orange"];
+
+let fruits = ["Apple", "Banana", "Orange", "Mango", "Grapes", "Pineapple"];
 console.log(fruits);
+
+//console.log(fruits[5]);
+
+//let myarray =[100,"wlcome",true,45.67,null,undefined];
+//console.log(myarray);
+
+let person1 = {
+    name : "ram",
+    age : 30,
+    city : "bangalore"
+}
+
+let person2 = {
+    name : "divaid",
+    age : 32,
+}
+
+let myarray =[person1, person2];
+console.log(myarray);
+
+//let myarray =[100,"wlcome",true,45.67,null,undefined];
+//read the array elements using for loop
+for(let i = 0; i <= myarray.length-1; i++){
+ 
+   console.log(myarray[i]);
+}
+ console.log();
+
+//read the array elements using for of loop
+for(let x of myarray){
+    console.log(x);
+}
+
+console.log(typeof myarray);
+console.log(Array.isArray(myarray));
